67ccb1726465e4fad8f85a4285def8033b888e97
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2019-04-23 21:18:49 +0600

    feat: edit makeToggleable HOC

--- app/src/HOC/makeToggleable.js
+++ app/src/HOC/makeToggleable.js
@@ -8,25 +8,48 @@ function makeToggleable(Component) {
   return class extends React.Component {
     static displayName = `Toggle(${getDisplayName(Component)})`;
 
-    state = {
-      isOpen: false,
+    constructor(props) {
+      super(props);
+      this.state = {
+        isOpen: false,
+      };
+
+      this.rootRef = React.createRef();
+    }
+
+    componentDidMount() {
+      document.addEventListener('mousedown', this.handleClickOutside);
+    }
+
+    componentWillUnmount() {
+      document.removeEventListener('mousedown', this.handleClickOutside);
+    }
+
+    handleClickOutside = event => {
+      if (this.rootRef.current && !this.rootRef.current.contains(event.target)) {
+        if (this.state.isOpen) {
+          this.toggle();
+        }
+      }
     };
 
     toggle = () => {
       this.setState(({ isOpen }) => ({ isOpen: !isOpen }));
-    }
+    };
+
+    open = () => {
+      this.setState({ isOpen: true });
+    };
+
+    close = () => {
+      this.setState({ isOpen: false });
+    };
 
     render() {
       const { isOpen } = this.state;
-      const { toggle } = this;
+      const { toggle, open, close, rootRef } = this;
 
-      return (
-        <Component
-          isOpen={isOpen}
-          toggle={toggle}
-          {...this.props}
-        />
-      );
+      return <Component {...this.props} {...{ isOpen, open, close, toggle, rootRef }} />;
     }
   };
 }
