ec584a3741ab58b05df589fe74d590066d421a2d
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2019-05-10 18:32:07 +0600

    feat: add icon creator

--- creators/index.js
+++ creators/index.js
@@ -3,12 +3,14 @@ const containerCreator = require('./containerCreator');
 const uiCreator = require('./uiCreator');
 const pageCreator = require('./pageCreator');
 const duckCreator = require('./duckCreator');
+const iconCreator = require('./iconCreator');
 const layoutCreator = require('./layoutCreator');
 
 const creators = {
   '--component': componentCreator,
   '--container': containerCreator,
   '--layout': layoutCreator,
+  '--icon': iconCreator,
   '--duck': duckCreator,
   '--page': pageCreator,
   '--ui': uiCreator,
