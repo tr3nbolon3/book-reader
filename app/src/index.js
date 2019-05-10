f020bcac2cc949019701d9c5112dc4c192d7d342
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2019-05-10 15:37:25 +0600

    fix: linter errors

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux"; // TODO: remove from this file
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux'; // TODO: remove from this file
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 // import ErrorBoundary from "./components/ErrorBoundary";
-import App from "./App";
-import "./index.css";
+import App from './App';
+import './index.css';
 
 // import configStore from "./store/configStore";
 
@@ -15,10 +15,10 @@ const store = createStore(state => state); // TODO: replace createStore to confi
 ReactDOM.render(
   <Provider store={store}>
     {/* <ErrorBoundary> */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
     {/* </ErrorBoundary> */}
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
