40f7e5b9a4f8868d81b61a19a407be362bb193b5
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2019-05-10 22:41:37 +0600

    fix: add aliases in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,24 @@ module.exports = {
     'plugin:react/recommended'
   ],
   env: {
-    browser: true
+    browser: true,
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ["@src", "./src"],
+          ["@ducks", "./src/store/ducks"],
+          ["@utils", "./src/utils"],
+          ["@components", "./src/views/components"],
+          ["@enhancers", "./src/views/components"],
+          ["@layouts", "./src/views/layouts"],
+          ["@pages", "./src/views/pages"],
+          ["@prop-types", "./src/views/prop-types"],
+          ["@UI", "./src/views/UI"]
+        ],
+      },
+    },
   },
 };
--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux'; // TODO: remove from this file
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
 // import ErrorBoundary from '~/views/components/ErrorBoundary';
 import App from './App';
 import './index.css';
 
 // import configStore from "./store/configStore";
-
 const store = createStore(state => state); // TODO: replace createStore to configStore
 
 ReactDOM.render(
