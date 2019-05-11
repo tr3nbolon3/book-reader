a6fcc3653b0bbfd313641f01d24912c14ea03df5
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2019-05-11 12:47:11 +0600

    feat: modified RefreshIcon

--- src/views/prop-types/index.js
+++ src/views/prop-types/index.js
@@ -2,6 +2,18 @@ import PropTypes from 'prop-types';
 
 const numberOrString = PropTypes.oneOfType([PropTypes.number, PropTypes.string]);
 
+const icon = PropTypes.shape({
+  width: numberOrString,
+  height: numberOrString,
+  fill: PropTypes.string,
+  style: PropTypes.object,
+  margin: PropTypes.string,
+  marginTop: numberOrString,
+  marginLeft: numberOrString,
+  marginRight: numberOrString,
+  marginBottom: numberOrString,
+});
+
 export default {
-  numberOrString,
+  icon,
 };
