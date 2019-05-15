63acd510c23ee9cc6a2f9aed2f20a7b04ef2fee4
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2019-05-15 17:01:46 +0600

    feat: implement footer

--- src/views/components/Footer/Footer.js
+++ src/views/components/Footer/Footer.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React from 'react';
+import { indigo } from '@material-ui/core/colors';
+import { Typography } from '@material-ui/core';
 
-class Footer extends React.Component {
-  render() {
-    return <React.Fragment />;
-  }
+import Logo from '@UI/Logo';
+import Link from '@UI/Link';
+
+import * as paths from '@routes/paths';
+
+import styles from './Footer.module.scss';
+
+const background = indigo[500];
+
+function Footer() {
+  return (
+    <div className={styles.root} style={{ background }}>
+      <Logo className={styles.logo} />
+      <div className={styles.links}>
+        <Link className={styles.link} to={paths.USER_AGREEMENT}>
+          Пользовательское соглашение
+        </Link>
+        <Link className={styles.link} to={paths.PRIVACY_POLICY}>
+          Политика конфиденциальности
+        </Link>
+        <Link className={styles.link} to={paths.PUBLIC_OFFER}>
+          Публичная оферта
+        </Link>
+        <Link className={styles.link} to={paths.CONTACTS}>
+          Контакты
+        </Link>
+      </div>
+      <Typography color="inherit">© bambook 2019</Typography>
+    </div>
+  );
 }
 
 export default Footer;
