04a189ebeb7f75227f3d43797003c1a5f401a587
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2019-05-15 17:06:01 +0600

    feat: implement MainLayout

--- src/views/layouts/MainLayout/MainLayout.js
+++ src/views/layouts/MainLayout/MainLayout.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function MainLayout({ children }) {
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+
+import styles from './MainLayout.module.scss';
+
+function MainLayout({ children, headerProps, footerProps }) {
   return (
-    <React.Fragment>
-      <div>Header</div>
-      <main>{children}</main>
-      <div>Footer</div>
-    </React.Fragment>
+    <div className={styles.root}>
+      <Header {...headerProps} />
+      <div className={styles.main}>{children}</div>
+      <Footer {...footerProps} />
+    </div>
   );
 }
 
+MainLayout.defaultProps = {
+  children: null,
+  headerProps: {},
+  footerProps: {},
+};
+
 MainLayout.propTypes = {
   children: PropTypes.any,
+  headerProps: PropTypes.shape(Header.propTypes),
+  footerProps: PropTypes.shape(Footer.propTypes),
 };
 
 export default MainLayout;
