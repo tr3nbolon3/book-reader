615b802966993ac847a911408139a3b62bd94c33
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2019-05-24 14:15:30 +0600

    refactor: path aliases

---
+++ aliases.js
@@ -0,0 +1,12 @@
+export default [
+  ['@src', 'src/'],
+  ['@components', 'src/views/components'],
+  ['@routes', 'src/routes'],
+  ['@ducks', 'src/store/ducks'],
+  ['@utils', 'src/utils'],
+  ['@enhancers', 'src/views/enhancers'],
+  ['@layouts', 'src/views/layouts'],
+  ['@pages', 'src/views/pages'],
+  ['@prop-types', 'src/views/prop-types'],
+  ['@UI', 'src/views/UI'],
+];
