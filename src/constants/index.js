d852a4201a4b61072691290a0c1e01a9ed842f58
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2019-05-27 23:26:46 +0600

    feat: add gender constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,9 @@
 import accessRestrictions from './accessRestrictions';
 import subscribes from './subscribes';
+import gender from './gender';
 
 export default {
+  gender,
   subscribes,
   accessRestrictions,
 };
