fb63ad95a0a4b97fc5c350ac8be7d566224795c6
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2019-05-28 00:41:55 +0600

    feat: firestoreSelectors

--- src/store/ducks/firestore/firestoreSelectors.js
+++ src/store/ducks/firestore/firestoreSelectors.js
@@ -1,8 +1,40 @@
 import { createSelector } from 'reselect';
 
-export const getFirestorePart = state => state.firestore.firestorePart;
+// USERS SELECTORS
+export const getDataUsers = state => state.firestore.firestore.data.users;
+export const getOrderedUsers = state => state.firestore.firestore.ordered.users;
 
-export const getFirestorePartFiltered = createSelector(
-  getFirestorePart,
-  firestorePart => firestorePart.filter(Boolean),
+// BOOK AUTHORS SELECTORS
+export const getDataAuthors = state => state.firestore.firestore.data.authors;
+export const getOrderedAuthors = state => state.firestore.firestore.ordered.authors;
+
+// BOOK COMMENTS SELECTORS
+export const getPureDataComments = state => state.firestore.firestore.data.comments;
+export const getPureOrderedComments = state => state.firestore.firestore.ordered.comments;
+
+export const getOrderedComments = createSelector(
+  [getPureOrderedComments, getDataUsers],
+  (comments, users) => comments.map(({ userId, ...comment }) => ({ user: users[userId], ...comment })),
+);
+
+// export const getDataComments = createSelector([]);
+export const getDataComments = state => getPureDataComments(state);
+
+// BOOK GENRES SELECTORS
+export const getDataGenres = state => state.firestore.firestore.data.genres;
+export const getOrderedGenres = state => state.firestore.firestore.ordered.genres;
+
+// BOOKS SELECTORS
+export const getDataBooks = state => state.firestore.firestore.data.books;
+export const getPureOrderedBooks = state => state.firestore.firestore.ordered.books;
+
+export const getOrderedBooks = createSelector(
+  [getPureOrderedBooks, getDataAuthors, getDataGenres, getOrderedComments],
+  (books, bookAuthors, bookGenres, allComments) =>
+    books.map(({ authorIds, genreIds, ...book }) => ({
+      genres: genreIds.map(id => ({ id, ...bookGenres[id] })),
+      authors: authorIds.map(id => ({ id, ...bookAuthors[id] })),
+      comments: allComments.filter(({ bookId }) => bookId === book.id),
+      ...book,
+    })),
 );
