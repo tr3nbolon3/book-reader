83751f3760a4d1e175d7514c03b9583e169b70ad
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2019-06-20 08:51:16 +0600

    done

--- src/store/ducks/firestore/actions/updateUser.js
+++ src/store/ducks/firestore/actions/updateUser.js
@@ -10,11 +10,14 @@ export const updateUserFailure = createAction(firestoreTypes.UPDATE_USER_FAILURE
 export const updateUser = formValues => async (dispatch, getState, { getFirestore }) => {
   dispatch(updateUserRequest());
   try {
-    const { firstName, lastName } = formValues;
-    const { uid: userId } = getUser(getState());
+    const user = getUser(getState());
+    const { uid: userId } = user;
+
+    const finalObj = { ...user, ...formValues };
+    const { firstName, lastName } = finalObj;
 
     const firestore = getFirestore();
-    await firestore.doc(`users/${userId}`).update({ ...formValues, fullName: `${firstName} ${lastName}` });
+    await firestore.doc(`users/${userId}`).update({ ...finalObj, fullName: `${firstName} ${lastName}` });
 
     dispatch(updateUserSuccess());
     dispatch(showNotification({ type: 'success', message: 'Данные обновлены' }));
